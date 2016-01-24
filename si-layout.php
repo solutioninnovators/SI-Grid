87ae5d0b04cf72dac420778b655e65eb39788555
thetuningspoon <mike@thetuningspoon.com>
2016-01-24 13:34:42 -0500

    Added prefixes and support for mobile-first mode

--- si-layout.php
+++ si-layout.php
@@ -6,6 +6,7 @@ header('Cache-Control: max-age=86400'); //Enable caching of the file
 /////////////////////////////////////////////////////////////////
 SI Layout - Auto-generated Responsive CSS Grid System (and more!)
 Developed by Mike Spooner for Solution Innovators
+v1.2
 See README for more information
 /////////////////////////////////////////////////////////////////
 */
@@ -14,6 +15,8 @@ See README for more information
 $gridSize = isset($_GET['gridSize']) && preg_match('/^\d{1,2}$/', $_GET['gridSize']) && $_GET['gridSize'] >= 2 ? $_GET['gridSize'] : 12;
 $gutterWidth = isset($_GET['gutterWidth']) && is_numeric($_GET['gutterWidth']) && strlen($_GET['gutterWidth']) <= 99 ? $_GET['gutterWidth'] : 4;
 $breakpoints = isset($_GET['breakpoints']) && preg_match('/^\d{1,4}(?:px|em)(?:\|\d{1,4}(?:px|em))*$/', $_GET['breakpoints']) ? $_GET['breakpoints'] : null;
+$prefix = isset($_GET['prefix']) && strlen($_GET['prefix']) < 50 && preg_match('/^[_a-zA-Z0-9-]{1,}$/', $_GET['prefix']) ? $_GET['prefix'] : null;
+$mode = isset($_GET['mode']) && is_int($_GET['mode']) ? $_GET['mode'] : null;
 
 
 // Create an array from the breakpoint string
@@ -21,14 +24,15 @@ $breakpointArray = is_null ($breakpoints) ? array() : explode('|', $breakpoints)
 
 // Sort array numerically in reverse order so that our responsive css rules cascade properly
 rsort($breakpointArray, SORT_NUMERIC);
+if($mode == 1) $breakpointArray = array_reverse($breakpointArray); // Reverse order for mobile-first mode
 
 ?>
 
 /* Clearfix */
-.grid:after,
-.gGrid:after,
-.container:after,
-.group:after { 
+.<?= $prefix ?>grid:after,
+.<?= $prefix ?>gGrid:after,
+.<?= $prefix ?>container:after,
+.<?= $prefix ?>group:after { 
 	content: ""; 
 	display: block; 
 	height: 0; 
@@ -36,7 +40,7 @@ rsort($breakpointArray, SORT_NUMERIC);
   visibility: hidden;
 }
 
-.nobr {
+.<?= $prefix ?>nobr {
 	white-space: nowrap;
 }
 
@@ -46,16 +50,16 @@ img {
 	border: 0;
 }
 
-.grid, .gGrid {
+.<?= $prefix ?>grid, .<?= $prefix ?>gGrid {
 	display: block;
 	zoom: 1; /* IE7 */
 	padding: 0;
 }
-.gGrid { 
+.<?= $prefix ?>gGrid { 
 	margin-left: -<?= $gutterWidth ?>%; /* Remove leftmost gutter */
 }
 
-.grid > *, .gGrid > * {
+.<?= $prefix ?>grid > *, .<?= $prefix ?>gGrid > * {
 	display: block;
 	float: left;
 	margin: 0;
@@ -63,7 +67,7 @@ img {
 	zoom: 1;
 	width: 100%; /* Set default width to span 12 columns */
 }
-.gGrid > * {
+.<?= $prefix ?>gGrid > * {
 	margin-left: <?= $gutterWidth ?>%; /* Gutter width */
 	width: <?= 100 - $gutterWidth ?>%;
 }
@@ -72,77 +76,77 @@ img {
 // Set our default grid without breakpoints
 for($colSpan=1; $colSpan<=$gridSize; $colSpan++) {
 ?>
-	.span<?= $colSpan ?> {width: <?= ($colSpan/$gridSize) * 100 ?>%;}
-	.gGrid > .span<?= $colSpan ?> {width: <?= ($colSpan/$gridSize) * 100 - $gutterWidth ?>%;}
+	.<?= $prefix ?>span<?= $colSpan ?> {width: <?= ($colSpan/$gridSize) * 100 ?>%;}
+	.<?= $prefix ?>gGrid > .<?= $prefix ?>span<?= $colSpan ?> {width: <?= ($colSpan/$gridSize) * 100 - $gutterWidth ?>%;}
 	
-	.append<?= $colSpan ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
-	.gGrid > .append<?= $colSpan ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
+	.<?= $prefix ?>append<?= $colSpan ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
+	.<?= $prefix ?>gGrid > .<?= $prefix ?>append<?= $colSpan ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
 	
-	.prepend<?= $colSpan ?> {margin-left: <?= ($colSpan/$gridSize) * 100 ?>%;}
-	.gGrid > .prepend<?= $colSpan ?> {margin-left: <?= ($colSpan/$gridSize) * 100 + $gutterWidth ?>%;}
+	.<?= $prefix ?>prepend<?= $colSpan ?> {margin-left: <?= ($colSpan/$gridSize) * 100 ?>%;}
+	.<?= $prefix ?>gGrid > .<?= $prefix ?>prepend<?= $colSpan ?> {margin-left: <?= ($colSpan/$gridSize) * 100 + $gutterWidth ?>%;}
 <?
 }
 ?>
 
-.spanHalf {width: <?= (1/2) * 100 ?>%;}
-.gGrid > .spanHalf {width: <?= (1/2) * 100 - $gutterWidth ?>%;}
-.spanThird {width: <?= (1/3) * 100 ?>%;}
-.gGrid > .spanThird {width: <?= (1/3) * 100 - $gutterWidth ?>%;}
-.spanFourth {width: <?= (1/4) * 100 ?>%;}
-.gGrid > .spanFourth {width: <?= (1/4) * 100 - $gutterWidth ?>%;}
-.spanFifth {width: <?= (1/5) * 100 ?>%;}
-.gGrid > .spanFifth {width: <?= (1/5) * 100 - $gutterWidth ?>%;}
+.<?= $prefix ?>spanHalf {width: <?= (1/2) * 100 ?>%;}
+.<?= $prefix ?>gGrid > .<?= $prefix ?>spanHalf {width: <?= (1/2) * 100 - $gutterWidth ?>%;}
+.<?= $prefix ?>spanThird {width: <?= (1/3) * 100 ?>%;}
+.<?= $prefix ?>gGrid > .<?= $prefix ?>spanThird {width: <?= (1/3) * 100 - $gutterWidth ?>%;}
+.<?= $prefix ?>spanFourth {width: <?= (1/4) * 100 ?>%;}
+.<?= $prefix ?>gGrid > .<?= $prefix ?>spanFourth {width: <?= (1/4) * 100 - $gutterWidth ?>%;}
+.<?= $prefix ?>spanFifth {width: <?= (1/5) * 100 ?>%;}
+.<?= $prefix ?>gGrid > .<?= $prefix ?>spanFifth {width: <?= (1/5) * 100 - $gutterWidth ?>%;}
 
-body .hide {display: none;}
-body .show {display: block;}
+body .<?= $prefix ?>hide {display: none;}
+body .<?= $prefix ?>show {display: block;}
 
-body .left {float:left;}
-body .right {float:right;}
-body .center {margin-left: auto; margin-right: auto;}
+body .<?= $prefix ?>left {float:left;}
+body .<?= $prefix ?>right {float:right;}
+body .<?= $prefix ?>center {margin-left: auto; margin-right: auto;}
 
-body .contentLeft {text-align:left;}
-body .contentRight {text-align:right;}
-body .contentCenter {text-align:center;}
+body .<?= $prefix ?>contentLeft {text-align:left;}
+body .<?= $prefix ?>contentRight {text-align:right;}
+body .<?= $prefix ?>contentCenter {text-align:center;}
 
 <?
 // Add classes for each breakpoint
 foreach($breakpointArray as $breakpoint) { 
 ?>
-	@media screen and (max-width:<?= $breakpoint ?>) {
+	@media (<? $mode == 1 ? 'min-width' : 'max-width' ?>:<?= $breakpoint ?>) {
 		<?
 		for($colSpan=1; $colSpan<=$gridSize; $colSpan++) {
 		?>
-			.span<?= $colSpan ?>_<?= $breakpoint ?> {width: <?= ($colSpan/$gridSize) * 100 ?>%;}
-			.gGrid .span<?= $colSpan ?>_<?= $breakpoint ?> {width: <?= ($colSpan/$gridSize) * 100 - $gutterWidth ?>%;}
+			.<?= $prefix ?>span<?= $colSpan ?>_<?= $breakpoint ?> {width: <?= ($colSpan/$gridSize) * 100 ?>%;}
+			.<?= $prefix ?>gGrid .<?= $prefix ?>span<?= $colSpan ?>_<?= $breakpoint ?> {width: <?= ($colSpan/$gridSize) * 100 - $gutterWidth ?>%;}
 			
-			.append<?= $colSpan ?>_<?= $breakpoint ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
-			.gGrid > .append<?= $colSpan ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
+			.<?= $prefix ?>append<?= $colSpan ?>_<?= $breakpoint ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
+			.<?= $prefix ?>gGrid > .<?= $prefix ?>append<?= $colSpan ?> {margin-right: <?= ($colSpan/$gridSize) * 100 ?>%;}
 			
-			.prepend<?= $colSpan ?>_<?= $breakpoint ?> {margin-left: <?= ($colSpan/$gridSize) * 100 ?>%;}
-			.gGrid > .prepend<?= $colSpan ?> {margin-left: <?= ($colSpan/$gridSize) * 100 + $gutterWidth ?>%;}
+			.<?= $prefix ?>prepend<?= $colSpan ?>_<?= $breakpoint ?> {margin-left: <?= ($colSpan/$gridSize) * 100 ?>%;}
+			.<?= $prefix ?>gGrid > .<?= $prefix ?>prepend<?= $colSpan ?> {margin-left: <?= ($colSpan/$gridSize) * 100 + $gutterWidth ?>%;}
 		<?
 		}
 		?>
 		
-		.spanHalf_<?= $breakpoint ?> {width: <?= (1/2) * 100 ?>%;}
-		.gGrid > .spanHalf_<?= $breakpoint ?> {width: <?= (1/2) * 100 - $gutterWidth ?>%;}
-		.spanThird_<?= $breakpoint ?> {width: <?= (1/3) * 100 ?>%;}
-		.gGrid > .spanThird_<?= $breakpoint ?> {width: <?= (1/3) * 100 - $gutterWidth ?>%;}
-		.spanFourth_<?= $breakpoint ?> {width: <?= (1/4) * 100 ?>%;}
-		.gGrid > .spanFourth_<?= $breakpoint ?> {width: <?= (1/4) * 100 - $gutterWidth ?>%;}
-		.spanFifth_<?= $breakpoint ?> {width: <?= (1/5) * 100 ?>%;}
-		.gGrid > .spanFifth_<?= $breakpoint ?> {width: <?= (1/5) * 100 - $gutterWidth ?>%;}
+		.<?= $prefix ?>spanHalf_<?= $breakpoint ?> {width: <?= (1/2) * 100 ?>%;}
+		.<?= $prefix ?>gGrid > .<?= $prefix ?>spanHalf_<?= $breakpoint ?> {width: <?= (1/2) * 100 - $gutterWidth ?>%;}
+		.<?= $prefix ?>spanThird_<?= $breakpoint ?> {width: <?= (1/3) * 100 ?>%;}
+		.<?= $prefix ?>gGrid > .<?= $prefix ?>spanThird_<?= $breakpoint ?> {width: <?= (1/3) * 100 - $gutterWidth ?>%;}
+		.<?= $prefix ?>spanFourth_<?= $breakpoint ?> {width: <?= (1/4) * 100 ?>%;}
+		.<?= $prefix ?>gGrid > .<?= $prefix ?>spanFourth_<?= $breakpoint ?> {width: <?= (1/4) * 100 - $gutterWidth ?>%;}
+		.<?= $prefix ?>spanFifth_<?= $breakpoint ?> {width: <?= (1/5) * 100 ?>%;}
+		.<?= $prefix ?>gGrid > .<?= $prefix ?>spanFifth_<?= $breakpoint ?> {width: <?= (1/5) * 100 - $gutterWidth ?>%;}
 		
-		body .hide_<?= $breakpoint ?> {display: none;}
-		body .show_<?= $breakpoint ?> {display: block;}
+		body .<?= $prefix ?>hide_<?= $breakpoint ?> {display: none;}
+		body .<?= $prefix ?>show_<?= $breakpoint ?> {display: block;}
 		
-		body .left_<?= $breakpoint ?> {float:left;}
-		body .right_<?= $breakpoint ?> {float:right;}
-		body .center_<?= $breakpoint ?> {margin-left: auto; margin-right: auto;}
+		body .<?= $prefix ?>left_<?= $breakpoint ?> {float:left;}
+		body .<?= $prefix ?>right_<?= $breakpoint ?> {float:right;}
+		body .<?= $prefix ?>center_<?= $breakpoint ?> {margin-left: auto; margin-right: auto;}
 		
-		body .contentLeft_<?= $breakpoint ?> {text-align:left;}
-		body .contentRight_<?= $breakpoint ?> {text-align:right;}
-		body .contentCenter_<?= $breakpoint ?> {text-align:center;}
+		body .<?= $prefix ?>contentLeft_<?= $breakpoint ?> {text-align:left;}
+		body .<?= $prefix ?>contentRight_<?= $breakpoint ?> {text-align:right;}
+		body .<?= $prefix ?>contentCenter_<?= $breakpoint ?> {text-align:center;}
 	}
 <?
 } 
